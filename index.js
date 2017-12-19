897248b6ecfc4c631b05c007bcadf9dc66d1a769
Guillaume Salicis <gsalicis@fastbooking.net>
2017-12-19 15:07:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const cheerio = require('cheerio');
+const got = require('got');
+
+const url = 'sindresorhus.com';
+
+got(url).then(content => parse(cheerio.load(content.body)))
+.then(d => console.log(d.join(',')));
+
+function parse(dom) {
+  const elts = dom('section.hero div#latest-repos-container h2');
+  return arrayzer(elts).map(elt => new MyObj(elt.firstChild.data));
+}
+
+function arrayzer(node) {
+  return Array.prototype.slice.apply(node)
+}
+
+
+function MyObj(str) {
+  this.name = str;
+}
+
+MyObj.prototype.toString = function rToString()
+{
+    return this.name.toString();
+}
